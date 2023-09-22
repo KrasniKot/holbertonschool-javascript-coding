28add7e9be29a5287b0efdd49edfc336f8bc36af
KrasniKot <6187@holbertonstudents.com>
2023-09-22 13:46:51 -0700

    task 1: argv index fixed

--- javascript-web_scraping/1-writeme.js
+++ javascript-web_scraping/1-writeme.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 
 const fs = require('fs');
-const f = process.argv[1];
-const d = process.argv[2];
+const f = process.argv[2];
+const d = process.argv[3];
 
 fs.writeFile(f, d, 'utf-8', (err, data) => { if (err) throw err; });
