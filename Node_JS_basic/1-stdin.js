9287c451b5f157dce6cd73581c2c6e9cc2f27f34
KrasniKot <emanueltrias9@gmail.com>
2023-10-10 16:46:03 +0000

    task 1 done

---
+++ Node_JS_basic/1-stdin.js
@@ -0,0 +1,8 @@
+console.log('Welcome to Holberton School, what is your name?');
+
+process.stdin.on('readable', () => {
+  const inpt = process.stdin.read();
+  if (inpt) {console.log(`Your name is: ${inpt.toString().trim()}`); }
+});
+
+process.stdin.on('end', () => { console.log('This important software is now closing'); });
