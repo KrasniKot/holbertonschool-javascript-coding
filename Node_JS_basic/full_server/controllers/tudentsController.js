4e79a49c97c75fc019fd60ba83c22a7102a10697
KrasniKot <emanueltrias9@gmail.com>
2023-10-14 17:59:34 +0000

    task 9 eslint and error code fixed

--- Node_JS_basic/full_server/controllers/tudentsController.js
+++ Node_JS_basic/full_server/controllers/tudentsController.js
@@ -12,8 +12,8 @@ module.exports = class StudentsController {
       }
 
       response.status(200).send(body.slice(0, -1));
-    } catch (_) {
-      response.status(500).send('Cannot load the database');
+    } catch (error) {
+      response.status(500).send();
     }
   }
 
@@ -21,15 +21,16 @@ module.exports = class StudentsController {
     const { major } = request.params;
     const accpt = ['CS', 'SWE'];
 
-    try {
-      if (!accpt.includes(major)) {
-        response.status(500).send('Major parameter must be CS or SWE');
-      } else {
-        const fields = await rD(process.argv[2]);
-        response.status(200).send(`List: ${fields[major].join(', ')}`);
-      }
-    } catch (_) {
-      response.status(500).send('Cannot load the database');
+    if (!accpt.includes(major)) {
+      response.status(500).send('Major parameter must be CS or SWE');
+    } else {
+      const fields = await rD(process.argv[2]);
+
+      response.status(200).send(`List: ${fields[major].join(', ')}`);
     }
+  }
+
+  catch(error) {
+    response.status(500).send();
   }
 };
