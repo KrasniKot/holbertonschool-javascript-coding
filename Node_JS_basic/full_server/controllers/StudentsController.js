795d1c355de4baeb250ebc49c5c9e5dd87077e70
KrasniKot <emanueltrias9@gmail.com>
2023-10-14 19:03:03 +0000

    task 9

--- Node_JS_basic/full_server/controllers/StudentsController.js
+++ Node_JS_basic/full_server/controllers/StudentsController.js
@@ -3,20 +3,20 @@ const rD = require('../utils');
 class StudentsController {
   static async getAllStudents(request, response) {
     try {
-      let body = 'This is the list of our students\n';
-
       const fields = await rD(process.argv[2]);
 
-      if (!fields || fields.length === 0) {
+      if (!fields || Object.keys(fields).length === 0) {
         return response.status(500).send('Cannot load the database');
       }
+
+      let body = 'This is the list of our students\n';
 
       for (const field of Object.keys(fields).sort()) {
         body += `Number of students in ${field}: ${fields[field].length}. List: ${fields[field].join(', ')}\n`;
       }
 
-      response.status(200).send(body.slice(0, -1));
-    } catch (_) {
+      response.status(200).send(body.trim());
+    } catch (error) {
       response.status(500).send('Cannot load the database');
     }
   }
@@ -31,13 +31,13 @@ class StudentsController {
       } else {
         const fields = await rD(process.argv[2]);
 
-        if (!fields || fields.length === 0) {
+        if (!fields || Object.keys(fields).length === 0 || !fields[major]) {
           return response.status(500).send('Cannot load the database');
         }
 
         response.status(200).send(`List: ${fields[major].join(', ')}`);
       }
-    } catch (_) {
+    } catch (error) {
       response.status(500).send('Cannot load the database');
     }
   }
